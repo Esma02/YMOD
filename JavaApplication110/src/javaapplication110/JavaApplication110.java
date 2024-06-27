94b89d2dc2fcf966c662f363e1ae70c3ba64e0a8
Esma02 <95567702+Esma02@users.noreply.github.com>
2024-06-28 01:38:48 +0300

    Add files via upload

---
+++ JavaApplication110/src/javaapplication110/JavaApplication110.java
@@ -0,0 +1,10 @@
+
+package javaapplication110;
+import java.util.Scanner;
+public class JavaApplication110 {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+      double []dizi = {5,7,9,14,6,10,100,65,56,74,2,0,4,-7,198};
+     
+               }  
+}
